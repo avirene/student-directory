1d00e82c5e5026232ec9a0ead08fb744e554fef5
Iryna Audzei <avirene@live.com>
2020-06-02 11:58:54 +0000

    Asking for both the name and the cohort

---
+++ exercise7.rb
@@ -0,0 +1,45 @@
+# In the input_students method the cohort value is hard-coded.
+# How can you ask for both the name and the cohort? What if one of the values is empty?
+# Can you supply a default value? The input will be given to you as a string?
+# How will you convert it to a symbol? What if the user makes a typo?
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Please enter the cohort"
+    cohort = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
