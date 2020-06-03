2f6ebcbc3b5adc3964abe324e7d0a27224566a52
Iryna Audzei <avirene@live.com>
2020-06-03 12:35:45 +0000

    Refactored the code to use a code block (each...end) to open and close a file

---
+++ exercise6a.rb
@@ -0,0 +1,110 @@
+@students = [] # an empty array accessible to all methods
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to a file"
+  puts "4. Load the list from a file"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    puts "Bye!"
+    exit # this will cause the program to terminate
+  when "3"
+    puts "Enter file name to save to"
+    save_students(STDIN.gets.chomp)
+  when "4"
+    puts "Enter file name to load from"
+    load_students(STDIN.gets.chomp)
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def add_hash_to_array(name, cohort)
+  @students << {name: name, cohort: cohort}
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    name, cohort = name.split(',')
+    add_hash_to_array(name, cohort)
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+  puts "Input complete"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students(filename)
+  # open the file for writing
+  File.open(filename, "w") {|file| @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    file.puts student_data.join(",")
+  end}
+  puts "List saved to #{filename}"
+end
+
+def load_students(filename)
+  File.open(filename, "r") {|file| file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    add_hash_to_array(name, cohort)
+  end}
+  puts "List loaded from #{filename}"
+end
+
+def try_load_students
+  filename = ARGV.first# first argument from the command line
+  if filename.nil?
+    load_students("students.csv")
+  elsif File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
+interactive_menu
